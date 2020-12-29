56368efe735e49b61ea8102538bed66dc8673534
Annie Huang <annie.hn@gmail.com>
2020-12-29 22:46:23 +1100

    Use args (a set of arguments (props, slots, inputs, etc)) for storybook template.

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -11,3 +11,25 @@ export const Primary = () => <Button variant='primary'>Primary</Button>;
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
 export const Success = () => <Button variant='success'>Success</Button>;
 export const Danger = () => <Button variant='danger'>Danger</Button>;
+
+// components with a lot more jsx, defining the args object is much better than duplicating the jsx.
+// you can also reuse the args for another story.
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+  variant: 'primary',
+  children: 'Primary Args',
+};
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA,
+  children: 'Long Primary Args',
+};
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: 'secondary',
+  children: 'Secondary Args',
+};
