95cf65b37ec48885c92cc78f0ce1199f77738f54
Annie Huang <annie.hn@gmail.com>
2020-12-30 10:22:09 +1100

    Use decorator to wrap every story in a file inside an extra component

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -9,28 +9,13 @@ export default {
   args: {
     children: 'Button',
   },
+  decorators: [(story) => <Center>{story()}</Center>],
 };
 
-export const Primary = () => (
-  <Center>
-    <Button variant='primary'>Primary</Button>
-  </Center>
-);
-export const Secondary = () => (
-  <Center>
-    <Button variant='secondary'>Secondary</Button>
-  </Center>
-);
-export const Success = () => (
-  <Center>
-    <Button variant='success'>Success</Button>
-  </Center>
-);
-export const Danger = () => (
-  <Center>
-    <Button variant='danger'>Danger</Button>
-  </Center>
-);
+export const Primary = () => <Button variant='primary'>Primary</Button>;
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
+export const Success = () => <Button variant='success'>Success</Button>;
+export const Danger = () => <Button variant='danger'>Danger</Button>;
 
 // components with a lot more jsx, defining the args object is much better than duplicating the jsx.
 // you can also reuse the args for another story.
