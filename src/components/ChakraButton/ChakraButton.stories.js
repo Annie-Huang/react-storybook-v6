cc969fd3be1b154b69e16b3d83ccae171e34844a
Annie Huang <annie.hn@gmail.com>
2020-12-30 18:21:28 +1100

    !!! Controls Addon so you can change the injected property.

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -4,7 +4,24 @@ import { Button } from '@chakra-ui/react';
 export default {
   title: 'Chakra/Button',
   component: Button,
+  argTypes: {
+    variantColor: { control: 'text' },
+    children: { control: 'text' },
+  },
 };
 
-export const Success = () => <Button variantColor='green'>Button</Button>;
-export const Danger = () => <Button variantColor='red'>Danger</Button>;
+const Template = (args) => <Button {...args} />;
+
+// export const Success = () => <Button variantColor='green'>Button</Button>;
+export const Success = Template.bind({});
+Success.args = {
+  variantColor: 'green',
+  children: 'Success',
+};
+
+// export const Danger = () => <Button variantColor='red'>Danger</Button>;
+export const Danger = Template.bind({});
+Danger.args = {
+  variantColor: 'red',
+  children: 'Danger',
+};
