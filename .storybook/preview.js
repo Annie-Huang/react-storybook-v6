69beb48ca28fbcaaf3aa434ebf5c295be2596cf4
Annie Huang <annie.hn@gmail.com>
2020-12-30 17:18:30 +1100

    Try theming. But the new version of ChakraButton doesn't seem to work with theming illustrated in the tutorial.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Center from '../src/components/Center/Center';
+import { ChakraProvider, extendTheme, Theme, CSSReset } from '@chakra-ui/react';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -20,3 +21,24 @@ export const decorators = [
     </Center>
   ),
 ];
+
+// Theming from ChakraProvider doesn't seem to work.
+// const colors = {
+//   colors: {
+//     brand: {
+//       900: '#1a365d',
+//       800: '#153e75',
+//       700: '#2a69ac',
+//     },
+//   },
+// };
+// const theme = extendTheme({ colors });
+// export const decorators = [
+//   (Story) => (
+//     <ChakraProvider theme={theme}>
+//       <Center>
+//         <Story />
+//       </Center>
+//     </ChakraProvider>
+//   ),
+// ];
