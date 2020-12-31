867b32ac252971c45ae0ddd8e16396605996b1b6
Annie Huang <annie.hn@gmail.com>
2020-12-31 15:45:15 +1100

    Clean up those addon that is already part of the @storybook/addon-essentials in main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,20 @@
 // You don't really have to add addon-docs, it's default added by the new version of Storybook.
+
+/* Essential has the following addon, so you don't need to add it
+https://storybook.js.org/docs/react/essentials/introduction
+Docs
+Controls
+Actions
+Viewport
+Backgrounds
+Toolbars & globals
+ */
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
-    '@storybook/addon-viewport',
-    '@storybook/addon-docs',
-    '@storybook/addon-controls',
     '@storybook/addon-knobs',
   ],
 };
